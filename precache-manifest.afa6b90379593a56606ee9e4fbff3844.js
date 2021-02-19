26f476c67c00ae63f4fa7dcc1c180583819835a7
Banx87 <joni.lindholm@gmail.com>
2021-02-19 14:00:47 +0200

    Updates

---
+++ precache-manifest.afa6b90379593a56606ee9e4fbff3844.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4428d27388d22e637431457f35c62772",
+    "url": "/robofriends-redux-pwa/index.html"
+  },
+  {
+    "revision": "c52f14444537fc8d7814",
+    "url": "/robofriends-redux-pwa/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "122a5b11529cddbca5d2",
+    "url": "/robofriends-redux-pwa/static/css/main.55c248dc.chunk.css"
+  },
+  {
+    "revision": "c52f14444537fc8d7814",
+    "url": "/robofriends-redux-pwa/static/js/2.28cd116b.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/robofriends-redux-pwa/static/js/2.28cd116b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "122a5b11529cddbca5d2",
+    "url": "/robofriends-redux-pwa/static/js/main.c6b49471.chunk.js"
+  },
+  {
+    "revision": "1cf1e5ed675ab949bd08",
+    "url": "/robofriends-redux-pwa/static/js/runtime-main.7eb93645.js"
+  }
+]);
