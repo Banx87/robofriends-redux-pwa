04da731c6b4840f89ebdbf29eb5614ee296a95db
Banx87 <joni.lindholm@gmail.com>
2021-02-22 14:08:57 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends-redux-pwa/precache-manifest.de80fc2c61478c0fbb88876d7faa27d9.js"
+  "/robofriends-redux-pwa/precache-manifest.f64cc752b3106404103d7a7f6d6b27f2.js"
 );
 
 self.addEventListener('message', (event) => {
