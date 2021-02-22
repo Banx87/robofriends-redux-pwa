b91a15ebfa3ec3d79082735e01f11ac826461914
Banx87 <joni.lindholm@gmail.com>
2021-02-22 14:31:32 +0200

    Font changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 import CounterBtn from '../CounterBtn/CounterBtn'
+import '../../index.css';
 
 class Header extends Component {
     // shouldComponentUpdate(nextProps, nextState) {
     //     return false;
     // }
     render() {
-        console.log("Header Rendered")
+        // console.log("Header Rendered")
         return (
         <div>
-        <h1 className="f1">RoboFriends Redux font change</h1>
+        <h1 className="f1">RoboFriends Redux</h1>
             <CounterBtn color={'red'}/>
         </div>
         )
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import './index.css';
-import './containers/app.css';
 import App from './App'
 import * as serviceWorker from './serviceWorker';
 import { searchRobots, requestRobots } from './reducers';
