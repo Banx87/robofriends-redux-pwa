a2d6468c3ebd8ee5b1219a53748196641c6321bd
Banx87 <joni.lindholm@gmail.com>
2021-08-26 13:02:55 +0300

    added pre CI prettier

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react'
-import CounterBtn from '../CounterBtn/CounterBtn'
-import '../../index.css';
+import React, { Component } from "react";
+import CounterBtn from "../CounterBtn/CounterBtn";
+import CounterBtn2 from "../CounterBtn2/CounterBtn2";
+import "../../index.css";
 
 class Header extends Component {
-    // shouldComponentUpdate(nextProps, nextState) {
-    //     return false;
-    // }
-    render() {
-        // console.log("Header Rendered")
-        return (
-        <div>
+  // shouldComponentUpdate(nextProps, nextState) {
+  //     return false;
+  // }
+  render() {
+    // console.log("Header Rendered")
+    return (
+      <div>
         <h1 className="f1">RoboFriends Redux</h1>
-            <CounterBtn color={'red'}/>
-        </div>
-        )
-    }
+        <CounterBtn color={"red"} />
+        <CounterBtn2 />
+      </div>
+    );
+  }
 }
 
 export default Header;
